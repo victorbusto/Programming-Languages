bed06d06089f43279cf6f0d4e769b96be39f85b0
victortpt <bustosoy@gmail.com>
2021-10-05 13:52:59 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Programming_Languages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
